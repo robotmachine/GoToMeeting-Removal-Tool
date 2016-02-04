9aaef69ed6a69affa3c8d802f7b23bc3601e5d03
Brian Carter <robotmachine@gmail.com>
2016-02-04 17:03:54 -0500

    Added log file.

--- g2m_rem.sh
+++ g2m_rem.sh
@@ -1,4 +1,19 @@
+#   ____     _____     __  __           _   _             
+#  / ___| __|_   _|__ |  \/  | ___  ___| |_(_)_ __   __ _ 
+# | |  _ / _ \| |/ _ \| |\/| |/ _ \/ _ \ __| | '_ \ / _` |
+# | |_| | (_) | | (_) | |  | |  __/  __/ |_| | | | | (_| |
+#  \____|\___/|_|\___/|_|  |_|\___|\___|\__|_|_| |_|\__, |
+#                                                   |___/ 
+#  ____                                _   _____           _ 
+# |  _ \ ___ _ __ ___   _____   ____ _| | |_   _|__   ___ | |
+# | |_) / _ \ '_ ` _ \ / _ \ \ / / _` | |   | |/ _ \ / _ \| |
+# |  _ <  __/ | | | | | (_) \ V / (_| | |   | | (_) | (_) | |
+# |_| \_\___|_| |_| |_|\___/ \_/ \__,_|_|   |_|\___/ \___/|_|
+#                                                            
+# Version 1.3.0
 # Created by Brian A Carter and Kyle Halversen
+#
+## Functions
 #
 # Creates a local function to move to trash instead of permanently deleting.
 function trash () {
@@ -15,32 +30,38 @@ function trash () {
       while [ -e ~/.Trash/"$dst" ]; do
         dst="`expr "$dst" : '\(.*\)\.[^.]*'` `date +%H-%M-%S`.`expr "$dst" : '.*\.\([^.]*\)'`"
       done
-      mv "$path" ~/.Trash/"$dst"
+      mv "$path" ~/.Trash/"$dst" && echo "$path moved to trash" || echo "Failed to trash $path"
     fi
   done
 }
 #
-## Make sure GoToMeeting and GoToMeeting Recording Manager are quit
-AppNames=("\"GoToMeeting\"" "\"GoToMeeting Recording Manager\"")
-for x in "${AppNames[@]}"
-do
-	osascript -e "quit app ${x}"
-done
+## Log File Comment
+logcomment() {
+	echo "" >> $logFile
+	echo "### $@ ###" >> $logFile
+}
+#
+## Establish a log file
+logFile=~/Library/Logs/com.citrixonline.g2mremoval.log
+echo "GoToMeeting Removal Tool .:. Log started $(date)\n" > $logFile
 #
 ## Delete GoToMeeting Plists
+logcomment "Delete Plists"
+#
 Plists=("com.citrixonline.GoToMeeting" "com.citrixonline.G2MUpdate" "com.citrixonline.mac.WebDeploymentApp")
 for x in "${Plists[@]}"
 do
-	defaults delete "$x" >/dev/null 2>&1 || echo
-	defaults -currentHost delete "$x" >/dev/null 2>&1 || echo
+	defaults delete "$x" >> $logFile 2>&1
+	defaults -currentHost delete "$x" >> $logFile 2>&1
 done
 #
 ## Delete GoToMeeting apps from Desktop, system Applications, and user Applications.
-mdfind -name GoToMeeting | grep -v Removal | grep -v ShareFile | grep .app | xargs -I {} trash {} >/dev/null 2>&1 || echo
-trash /Applications/GoToMeeting* >/dev/null 2>&1 || echo
-trash ~/Applications/GoToMeeting* >/dev/null 2>&1 || echo
-trash ~/Desktop/GoToMeeting* >/dev/null 2>&1 || echo
-trash ~/Library/Application\ Support/CitrixOnline/GoToMeeting* >/dev/null 2>&1 || echo
+logcomment "Trash Using MDFind"
+mdfind -name GoToMeeting | grep -v Removal | grep -v ShareFile | grep .app | xargs -I {} trash {} >> $logFile 2>&1
+trash /Applications/GoToMeeting* >> $logFile 2>&1
+trash ~/Applications/GoToMeeting* >> $logFile 2>&1
+trash ~/Desktop/GoToMeeting* >> $logFile 2>&1
+trash ~/Library/Application\ Support/CitrixOnline/GoToMeeting* >> $logFile 2>&1
 #
 ## Delete Launcher
-trash ~/Library/Application\ Support/CitrixOnline/CitrixOnlineLauncher.app >/dev/null 2>&1 || echo
+trash ~/Library/Application\ Support/CitrixOnline/CitrixOnlineLauncher.app >> $logFile 2>&1
