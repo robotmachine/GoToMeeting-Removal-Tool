ff90a7a2c00e01e24f57a7eec310a149ee8692cb
Brian Carter <robotmachine@gmail.com>
2016-09-29 14:42:15 -0400

    Sierra

--- g2m_rem.sh
+++ g2m_rem.sh
@@ -10,7 +10,7 @@
 # |  _ <  __/ | | | | | (_) \ V / (_| | |   | | (_) | (_) | |
 # |_| \_\___|_| |_| |_|\___/ \_/ \__,_|_|   |_|\___/ \___/|_|
 #                                                            
-# Version 1.3.3
+# Version 1.3.4
 # Created by Brian A Carter and Kyle Halversen
 #
 ## Functions
